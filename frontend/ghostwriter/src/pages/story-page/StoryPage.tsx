ca85b3242f6e1546576339871e551632d3c6d809
lenrion <elena_henrion@brown.edu>
2023-01-29 10:59:12 -0500

    broken

--- frontend/ghostwriter/src/pages/story-page/StoryPage.tsx
+++ frontend/ghostwriter/src/pages/story-page/StoryPage.tsx
@@ -3,7 +3,6 @@ import { Stack, Heading, Image, Button, Box, Text, Flex, Show} from '@chakra-ui/
 import Typewriter from 'typewriter-effect';
 import PostPage from '../post-page/PostPage'
 import ChoiceComponant from "../choice-component/ChoiceComponent";
-<<<<<<< HEAD
 import {dalle, GptCall, callType} from "../../OpenaiHandlers"
 import { userDataMap } from "../post-page/PostPage";
 
@@ -18,15 +17,6 @@ function StoryPage() {
   const caller = new GptCall(userDataMap.get("names"), userDataMap.get("location"), userDataMap.get("storyType"));
   const response = caller.call(callType.START)
     // const response = `
-=======
-import {dalle, gpt, StoryType } from "../../OpenaiHandlers"
-import { eventWrapper } from "@testing-library/user-event/dist/utils";
-
-
-function StoryPage() {
-    const [response, setResponse] = useState<string>(`
->>>>>>> a3c2fd770ec968fa36884a906e7cccbc2b5b0c9d
-
     // John and Jane had been childhood friends, but ever since they had grown up, their paths had hardly crossed. So when Jane called John out of the blue to ask him to join her and her friend Jill for a camping trip in the woods, John eagerly accepted.
     
     // The three of them set off in the early morning, and despite the chilly air, they were in high spirits. It wasn’t long before they arrived at their destination. They set up camp, and before long, they were all gathered around the campfire, telling stories and roasting marshmallows.
@@ -47,11 +37,7 @@ function StoryPage() {
     
     // Without another word, the three of them turned and ran as fast as they could, not stopping until they made it back to the safety of their car.
     
-<<<<<<< HEAD
     // John and Jane never spoke of the incident again, but ever since that night, they have both had a feeling that the woman in white was still out there in the woods, watching them from the shadows.`
-=======
-    John and Jane never spoke of the incident again, but ever since that night, they have both had a feeling that the woman in white was still out there in the woods, watching them from the shadows.`);
->>>>>>> a3c2fd770ec968fa36884a906e7cccbc2b5b0c9d
     
     const [source, setSource] = useState<string>(""); 
     const [isTyping, setIsTyping] = useState<boolean>(true);
@@ -101,13 +87,8 @@ function StoryPage() {
               key={response}
               onInit={(typewriter) => {
                 setIsTyping(true);
-<<<<<<< HEAD
                 response.then( (res) =>{
                 res.split('\n')
-=======
-                // Parses reponse into paragraphs
-                response.split('\n')
->>>>>>> a3c2fd770ec968fa36884a906e7cccbc2b5b0c9d
                         .map((par) => par.trim())
                         .filter((par) => {
                           if (!(par === "")) {
@@ -119,18 +100,13 @@ function StoryPage() {
                         .forEach((par) => {
                           typewriter.callFunction(() => { callDallE(par) }) 
                           typewriter.typeString(par)
-<<<<<<< HEAD
                           .pauseFor(1000)
                           .deleteAll()
                           .start();
                         })})
-=======
-                          typewriter.deleteAll()
-                          typewriter.start();
->>>>>>> a3c2fd770ec968fa36884a906e7cccbc2b5b0c9d
                 setIsTyping(false);
                 }
-              )}}
+              }
               options = {{
                 deleteSpeed: 0,
                 delay: 75,
