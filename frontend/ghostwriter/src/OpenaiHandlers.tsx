14b2cf63cc429e70b9fd91129d058ce7336ce6b5
cbobowic <colden_bobowick@brown.edu>
2023-01-28 19:29:39 -0500

    working on gpt parsing

--- frontend/ghostwriter/src/OpenaiHandlers.tsx
+++ frontend/ghostwriter/src/OpenaiHandlers.tsx
@@ -33,12 +33,13 @@ async function dalle(prompt: string) {
     return generateImage.data.data[0].url;
 }
 
-function text(people: Person[], location: string, storyType: StoryType) {
+async function text(people: Person[], location: string, storyType: StoryType) {
    
     const prompt = "Write a ghost story involving the following people: " + 
         people.map(person => person.name + " (" + person.pronouns + ")").join(", ") + ". " + // list the people (pro/nouns)
         "The story should take place in " + location + ". " + // where the story takes place
-        "The story should be " + storyType + "."; // what type of story it is
+        "The story should be " + storyType + ". " + // what type of story it is
+        "The story should be around a page. ";
 
     console.log(prompt);
 
@@ -48,25 +49,27 @@ function text(people: Person[], location: string, storyType: StoryType) {
 
   const openai = new OpenAIApi(configuration);
 
-  return async () => {
-      await openai.createCompletion({
+  const result = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: generatePrompt(prompt),
+        prompt: prompt,
         temperature: 0.6,
+        max_tokens: 2048,
       });
+
+    return result.data.choices[0].text;
     };
-}
 
-function generatePrompt(prompt: string) {
-  const lower = prompt[0].toUpperCase() + prompt.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
 
-  Animal: Cat
-  Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-  Animal: Dog
-  Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-  Animal: ${lower}
-  Names:`;
-}
+// function generatePrompt(prompt: string) {
+//   const lower = prompt[0].toUpperCase() + prompt.slice(1).toLowerCase();
+//   return `Suggest three names for an animal that is a superhero.
+
+//   Animal: Cat
+//   Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+//   Animal: Dog
+//   Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+//   Animal: ${lower}
+//   Names:`;
+// }
 
 export { dalle, text, StoryType, Person }
